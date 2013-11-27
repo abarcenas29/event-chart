d2f6ab6815fd787422e127d2e8a05d42ab5b6052
aldrich barcenas <aldrich.barcenas@gmail.com>
2013-11-27 15:42:41 +0800

    social diag

--- fuel/app/views/admin/dashboard/social.php
+++ fuel/app/views/admin/dashboard/social.php
@@ -1,6 +1,12 @@
 <section class="uk-margin-top
 		 ec-admin-container
 		 uk-container-center">
+<?php foreach($content['data'] as $row): ?>
+<pre>
+<?php print $row; ?>
+</pre>
+<?php endforeach;?>
+	
 <pre>
 <?php 
 	print_r($content);
