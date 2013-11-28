3380e6690460f1114e3b0a63e436b1c0764330e3
aldrich barcenas <aldrich.barcenas@gmail.com>
2013-11-28 18:43:32 +0800

    model corrected

--- fuel/app/tasks/preview.php
+++ fuel/app/tasks/preview.php
@@ -6,7 +6,7 @@ class preview
 	public static function run($day)
 	{
 		\Fuel::$env = \Fuel::STAGING;
-		$q = Model_chart::event_preview($day);
+		$q = \Model_chart::event_preview($day);
 		
 		\Log::info('Preview Operation Starting');
 		
@@ -27,8 +27,8 @@ class preview
 				$arg['content'] = "Heads UP! There are $qty Event(s) lined up for the next $day days.";
 			}
 			$arg['url'] = Uri::create("chart/preview/$day");
-			Model_broadcast::post_twitter($arg);
-			Model_broadcast::post_twitter($arg);
+			\Model_broadcast::post_twitter($arg);
+			\Model_broadcast::post_twitter($arg);
 		}
 		\Log::info("Preview Operation Resulted in $qty Events posted. for entry $day");
 	}
