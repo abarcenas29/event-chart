a660d8447ea456a8d1b70807c241720c5151b99a
aldrich barcenas <aldrich.barcenas@gmail.com>
2013-11-29 18:21:48 +0800

    remove limitier on pagination

--- fuel/app/classes/model/event/list.php
+++ fuel/app/classes/model/event/list.php
@@ -89,7 +89,6 @@ class Model_Event_list extends Model_ModelCore
 	public static function admin_index_event($page)
 	{
 		$limit	= 15;
-		$page	= abs($page)-1;
 		
 		$q			= Model_Event_list::query()
 						->order_by('start_at','desc');
