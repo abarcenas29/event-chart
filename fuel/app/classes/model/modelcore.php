7fe4a033444265800c2880a72c582cf73a4c9c01
aldrich barcenas <aldrich.barcenas@gmail.com>
2013-12-02 15:38:54 +0800

    correct timestamp

--- fuel/app/classes/model/modelcore.php
+++ fuel/app/classes/model/modelcore.php
@@ -4,8 +4,8 @@ class Model_ModelCore extends Orm\Model
 	protected static function _get_timestamp($event_id)
 	{
 		$event = Model_Event_list::read_public_list($event_id);
-		$time  = new DateTime(strtotime($event['start_at'].' 23:59:59'));
-		return $time->getTimestamp();
+		$time  = strtotime($event['start_at'].' 23:59:59');
+		return $time;
 	}
 }
 
