095837d998697d6f5ada4e6b4eadd49eca07459e
Aldrich Allen Barcenas <aldrich.barcenas@gmail.com>
2014-09-26 12:05:52 +0800

    jsonp implementation

--- fuel/app/classes/controller/api/vendor.php
+++ fuel/app/classes/controller/api/vendor.php
@@ -6,12 +6,21 @@ class Controller_Api_Vendor extends Controller_Api_ApiCore
 {
     //events that will happen this past 6 months
     //city = filter the events by city/region
-    public function get_events()
+    public function post_events()
+    {   
+        $city = Input::post('city','all');
+        $c = Model_chart::format_chart($city);
+        return $this->response($c);
+    }
+    
+    public function get_events2()
     {
+        header('content-type: application/json; charset=utf-8');
+        
         $city = Input::get('city','all');
         $c = Model_chart::format_chart($city);
         
-        print $_GET['callback'] . json_encode($c);
+        print $_GET['callback'] .'(' . json_encode($c).')';
     }
     
     //events that is happening today
